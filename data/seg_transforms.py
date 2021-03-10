eb985dcfbf7f03d7f8f625522c6582eee9a96725
JIAXUAN <54315224+Jiaxuan-Li@users.noreply.github.com>
2021-03-10 10:03:44 +0800

    Add files via upload

---
+++ data/seg_transforms.py
@@ -0,0 +1,75 @@
+import numbers
+import random
+import numpy as np
+from PIL import Image, ImageOps
+import torch
+
+class Label_Transform(object):
+    def __init__(self,label_pixel=(26, 51, 77, 102, 128, 153, 179, 204, 230, 255)):
+        self.label_pixel = label_pixel
+
+    def __call__(self, image, label, *args):
+        label = np.array(label)
+        for i in range(len(self.label_pixel)):
+            label[label == self.label_pixel[i]] = i+1
+
+        return image,Image.fromarray(label)
+
+class Normalize(object):
+    """Given mean: (R, G, B) and std: (R, G, B),
+    will normalize each channel of the torch.*Tensor, i.e.
+    channel = (channel - mean) / std
+    """
+
+    def __init__(self, mean, std):
+        self.mean = torch.FloatTensor(mean)
+        self.std = torch.FloatTensor(std)
+
+    def __call__(self, image, label=None):
+        for t, m, s in zip(image, self.mean, self.std):
+            t.sub_(m).div_(s)
+        if label is None:
+            return image,
+        else:
+            return image, label
+
+
+class ToTensor(object):
+    """Converts a PIL.Image or numpy.ndarray (H x W x C) in the range
+    [0, 255] to a torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0].
+    """
+
+    def __call__(self, pic, label=None):
+        if isinstance(pic, np.ndarray):
+            # handle numpy array
+            img = torch.from_numpy(pic)
+        else:
+            # handle PIL Image
+            img = torch.ByteTensor(torch.ByteStorage.from_buffer(pic.tobytes()))
+            # PIL image mode: 1, L, P, I, F, RGB, YCbCr, RGBA, CMYK
+            if pic.mode == 'YCbCr':
+                nchannel = 3
+            else:
+                nchannel = len(pic.mode)
+            img = img.view(pic.size[1], pic.size[0], nchannel)
+            # put it from HWC to CHW format
+            # yikes, this transpose takes 80% of the loading time/CPU
+            img = img.transpose(0, 1).transpose(0, 2).contiguous()
+        img = img.float().div(255)
+        if label is None:
+            return img,
+        else:
+            return img, torch.LongTensor(np.array(label, dtype=np.int))
+
+
+class Compose(object):
+    """Composes several transforms together.
+    """
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, *args):
+        for t in self.transforms:
+            args = t(*args)
+        return args
